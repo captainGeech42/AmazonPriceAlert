8c525c4eac533cd92f59874b0b155e28bbf73157
Zander Work <zander@zanderwork.com>
2017-01-19 13:49:11 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+##Amazon Price Alert script##
+
+This script will send you an email when the price for a specific item on Amazon drops beneath a certain threshold
+
+Dependencies:
+ * Python 2
+ * `BeautifulSoup4` (the `pip` package is called `bs4`)
+ * `requests`
+ * `smtplib`
+ * `MIMEText`
+ * `ConfigParser`
+
+Usage:
+ 1. Duplicate `config_example.ini` and rename it to `config.ini`
+ 2. Add correct Amazon product settings to `config.ini`
+ 3. Add correct email server settings to `config.ini`
+ 4. Run the script via `python amazon_price.py`
+ 5. You may wish to add this to `crontab` (*nix) or configure a `Scheduled Task` (Windows)
---
+++ amazon_price.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+from email.mime.text import MIMEText
+import ConfigParser
+
+def logOutput(output):
+	print '[PRICEALERT]: {0}'.format(output)
+
+#TARGET_PRICE = 750
+#AMAZON_LINK = 'http://www.amazon.com/Dell-Inspiron-i7559-2512BLK-Generation-GeForce/dp/B015PYYDMQ'
+#AMAZON_PRODUCT = 'Dell Inspiron i7559'
+
+config = ConfigParser.ConfigParser()
+config.read('config.ini')
+
+TARGET_PRICE = int(config.get('AmazonSettings', 'TARGET_PRICE'))
+AMAZON_LINK = config.get('AmazonSettings', 'AMAZON_LINK')
+AMAZON_PRODUCT = config.get('AmazonSettings', 'AMAZON_PRODUCT')
+
+SENDER_EMAIL = config.get('EmailSettings', 'SENDER_EMAIL')
+SENDER_PASSWORD = config.get('EmailSettings', 'SENDER_PASSWORD')
+SMTP_SERVER = config.get('EmailSettings', 'SMTP_SERVER')
+SMTP_PORT = int(config.get('EmailSettings', 'SMTP_PORT'))
+
+request_page = requests.get(AMAZON_LINK)
+
+soup = BeautifulSoup(request_page.text, 'html.parser')
+
+amazon_price_obj = soup.find('span', 'a-size-base a-color-price offer-price a-text-normal', recursive=True)
+
+try:
+	#if the price was unable to be found, amazon_price_obj is type 'NoneType', and the below command will throw an exception
+	AMAZON_PRICE = float(amazon_price_obj.string.split('$')[1])
+except AttributeError:
+	logOutput('Price unavailable')
+	AMAZON_PRICE = TARGET_PRICE + 1
+
+if (AMAZON_PRICE < TARGET_PRICE):
+	logOutput('Price dropped! Sending email...')
+	msg = MIMEText('The price for the {0} has dropped to ${1}.\r\n\r\nYou can view the product listing here: {2}'.format(AMAZON_PRODUCT, str(AMAZON_PRICE), AMAZON_LINK))
+	msg['Subject'] = 'Automated Price Alert'
+	msg['From'] = SENDER_EMAIL
+	msg['To'] = RECEPIENT_EMAIL
+	smtp = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+	smtp.login(SENDER_EMAIL, SENDER_PASSWORD)
+	smtp.sendmail(SENDER_EMAIL, RECEPIENT_EMAIL, msg.as_string())
+	smtp.quit()
+	logOutput('Email sent!')
+else:
+	logOutput('Price didn\'t drop yet')
